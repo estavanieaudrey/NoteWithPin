3bff9767d1de00ead7d649138d80d54a6c66f594
estavanieaudrey <esta.audrey@gmail.com>
2024-06-27 23:29:58 +0700

    coba"

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,166 @@
+// import 'package:flutter/material.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+// import 'storage.dart';
+// import 'detailNote.dart';
+// import 'pinPage.dart';
+// import 'package:intl/intl.dart';
+
+// class HomePage extends StatelessWidget {
+//   final Box<Note> notesBox = Hive.box<Note>('notes');
+
+//   //navigasi untuk menuju ke tambah catatan baru
+//   void _addNote(BuildContext context) {
+//     Navigator.push(context, MaterialPageRoute(builder: (_) => NoteDetailPage()));
+//   }
+
+//   // ada card yang berguna untuk menanyakan apakah mau ganti pin / tidak
+//   void _showChangePinDialog(BuildContext context) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         // return nya dibuat alert
+//         return AlertDialog(
+//           title: Text('Ubah PIN'),
+//           content: Text('Apakah Anda ingin mengubah PIN?'),
+//           actions: [
+//             // kl ga brarti kembali ke page homepage.dart
+//             TextButton(
+//               child: Text('Tidak'),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//             // kl iya brarti manggil function changepin yang direct ke pinpage 
+//             TextButton(
+//               child: Text('Ya'),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//                 _changePin(context);
+//               },
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   // mengarahkan ke halaman pin page untuk ubah pin
+//   void _changePin(BuildContext context) {
+//     PinStorage.deletePin().then((_) {
+//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => PinPage()));
+//     });
+//   }
+
+// //   void _changePin(BuildContext context) {
+// //   Navigator.pushReplacement(
+// //     context,
+// //     MaterialPageRoute(builder: (_) => PinPage(isChangingPin: true)),
+// //   );
+// // }
+
+
+//   // mengarahkan ke halaman pinpage krn kalau logout berarti hrs isi pin lagi
+//   void _logout(BuildContext context) {
+//     Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => PinPage()));
+//   }
+
+//   // set format waktu hingga detik
+//   String _formatDateTime(DateTime dateTime) {
+//     return DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       // menu utama
+//       appBar: AppBar(
+//         title: Text('Catatan'),
+//         actions: [
+
+//           // ada menu pilihan mau logout atau ganti pin
+//           PopupMenuButton<String>(
+//             onSelected: (String value) {
+//               // kl semisal pilih logout,
+//               if (value == 'logout') {
+//                 // akan memanggil function _logout
+//                 _logout(context);
+//               // kl semisal pilih change pin
+//               } else if (value == 'change_pin') {
+//                 // akan memanggil function yang dapat mengubah pin
+//                 _showChangePinDialog(context);
+//               }
+//             },
+//             itemBuilder: (BuildContext context) {
+//               return [
+//                 PopupMenuItem<String>(
+//                   // value ini hrs sama kayak yg ada di loop
+//                   value: 'logout',
+//                   child: Text('Logout'),
+//                 ),
+//                 PopupMenuItem<String>(
+//                   // ini juga
+//                   value: 'change_pin',
+//                   child: Text('Ganti PIN'),
+//                 ),
+//               ];
+//             },
+//           ),
+//         ],
+//       ),
+//       body: ValueListenableBuilder(
+//         valueListenable: notesBox.listenable(),
+//         builder: (context, Box<Note> box, _) {
+//           // cek dari notesBox hive, klo kosong berarti ada tulisan tidak ada catatan
+//           if (box.values.isEmpty) {
+//             return Center(child: Text('Tidak ada catatan.'));
+//           }
+
+//           // Mengurutkan catatan berdasarkan waktu terakhir diedit
+//           final notes = box.keys.cast<int>().map((key) => MapEntry(key, box.get(key)!)).toList()
+//             ..sort((a, b) => b.value.lastEditedDate.compareTo(a.value.lastEditedDate));
+
+//           // kl trnyt didalam hive ada catetan :
+//           return ListView.builder( 
+//             // var notes yang isinya catatan yg sdh di sort dimasukkan ke itemCount untuk length nya
+//             itemCount: notes.length,
+//             // manggil konteks dan index item yang ada
+//             itemBuilder: (context, index) {
+//               // var note untuk menyimpan list notes pada index
+//               final note = notes[index].value;
+//               // var key untuk mengidentifikasi ini note yang mana
+//               final key = notes[index].key;
+//               return Card(
+//                 elevation: 4,
+//                 margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+//                 child: ListTile(
+//                   // menampilkan judul dari catatan 
+//                   title: Text(note.title),
+//                   // menampilkan format waktu yang udah di set berdasarkan datetime saat ini
+//                   subtitle: Text('Terakhir diedit: ${_formatDateTime(note.lastEditedDate)}'),
+//                   onTap: () {
+//                     // ketika card di klik, akan direct ke detailNote sesuai dengan index yang ada
+//                     Navigator.push(context, MaterialPageRoute(
+//                       builder: (_) => NoteDetailPage(note: note, noteKey: key),
+//                     ));
+//                   },
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: () => _addNote(context),
+//         backgroundColor: Colors.blueAccent,
+//       ),
+//     );
+//   }
+// }
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'storage.dart';
@@ -8,29 +171,24 @@ import 'package:intl/intl.dart';
 class HomePage extends StatelessWidget {
   final Box<Note> notesBox = Hive.box<Note>('notes');
 
-  //navigasi untuk menuju ke tambah catatan baru
   void _addNote(BuildContext context) {
     Navigator.push(context, MaterialPageRoute(builder: (_) => NoteDetailPage()));
   }
 
-  // ada card yang berguna untuk menanyakan apakah mau ganti pin / tidak
   void _showChangePinDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        // return nya dibuat alert
         return AlertDialog(
           title: Text('Ubah PIN'),
           content: Text('Apakah Anda ingin mengubah PIN?'),
           actions: [
-            // kl ga brarti kembali ke page homepage.dart
             TextButton(
               child: Text('Tidak'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
-            // kl iya brarti manggil function changepin yang direct ke pinpage 
             TextButton(
               child: Text('Ya'),
               onPressed: () {
@@ -44,27 +202,17 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  // mengarahkan ke halaman pin page untuk ubah pin
   void _changePin(BuildContext context) {
-    PinStorage.deletePin().then((_) {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => PinPage()));
-    });
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => PinPage(isChangingPin: true)),
+    );
   }
 
-//   void _changePin(BuildContext context) {
-//   Navigator.pushReplacement(
-//     context,
-//     MaterialPageRoute(builder: (_) => PinPage(isChangingPin: true)),
-//   );
-// }
-
-
-  // mengarahkan ke halaman pinpage krn kalau logout berarti hrs isi pin lagi
   void _logout(BuildContext context) {
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => PinPage()));
   }
 
-  // set format waktu hingga detik
   String _formatDateTime(DateTime dateTime) {
     return DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime);
   }
@@ -72,33 +220,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // menu utama
       appBar: AppBar(
         title: Text('Catatan'),
         actions: [
-
-          // ada menu pilihan mau logout atau ganti pin
           PopupMenuButton<String>(
             onSelected: (String value) {
-              // kl semisal pilih logout,
               if (value == 'logout') {
-                // akan memanggil function _logout
                 _logout(context);
-              // kl semisal pilih change pin
               } else if (value == 'change_pin') {
-                // akan memanggil function yang dapat mengubah pin
                 _showChangePinDialog(context);
               }
             },
             itemBuilder: (BuildContext context) {
               return [
                 PopupMenuItem<String>(
-                  // value ini hrs sama kayak yg ada di loop
                   value: 'logout',
                   child: Text('Logout'),
                 ),
                 PopupMenuItem<String>(
-                  // ini juga
                   value: 'change_pin',
                   child: Text('Ganti PIN'),
                 ),
@@ -110,35 +249,25 @@ class HomePage extends StatelessWidget {
       body: ValueListenableBuilder(
         valueListenable: notesBox.listenable(),
         builder: (context, Box<Note> box, _) {
-          // cek dari notesBox hive, klo kosong berarti ada tulisan tidak ada catatan
           if (box.values.isEmpty) {
             return Center(child: Text('Tidak ada catatan.'));
           }
 
-          // Mengurutkan catatan berdasarkan waktu terakhir diedit
           final notes = box.keys.cast<int>().map((key) => MapEntry(key, box.get(key)!)).toList()
             ..sort((a, b) => b.value.lastEditedDate.compareTo(a.value.lastEditedDate));
 
-          // kl trnyt didalam hive ada catetan :
-          return ListView.builder( 
-            // var notes yang isinya catatan yg sdh di sort dimasukkan ke itemCount untuk length nya
+          return ListView.builder(
             itemCount: notes.length,
-            // manggil konteks dan index item yang ada
             itemBuilder: (context, index) {
-              // var note untuk menyimpan list notes pada index
               final note = notes[index].value;
-              // var key untuk mengidentifikasi ini note yang mana
               final key = notes[index].key;
               return Card(
                 elevation: 4,
                 margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 child: ListTile(
-                  // menampilkan judul dari catatan 
                   title: Text(note.title),
-                  // menampilkan format waktu yang udah di set berdasarkan datetime saat ini
                   subtitle: Text('Terakhir diedit: ${_formatDateTime(note.lastEditedDate)}'),
                   onTap: () {
-                    // ketika card di klik, akan direct ke detailNote sesuai dengan index yang ada
                     Navigator.push(context, MaterialPageRoute(
                       builder: (_) => NoteDetailPage(note: note, noteKey: key),
                     ));
@@ -157,3 +286,4 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
--- lib/pinPage.dart
+++ lib/pinPage.dart
@@ -1,20 +1,120 @@
+// import 'package:flutter/material.dart';
+// import 'storage.dart';
+// import 'homepage.dart';
+
+// class PinPage extends StatefulWidget {
+//   @override
+//   _PinPageState createState() => _PinPageState();
+// }
+
+// class _PinPageState extends State<PinPage> {
+//   // controller untuk textfield input pin
+//   final TextEditingController _pinController = TextEditingController();
+//   // untuk cek apakah bikin baru? kl ngga brarti
+//   bool _isCreatingPin = false;
+
+//   // ini, storedpin : masukin pin yang sudah ada
+//   String? _storedPin;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkStoredPin();
+//   }
+
+//   // pengecekan apakah pin nya null?
+//   Future<void> _checkStoredPin() async {
+//     _storedPin = await PinStorage.getPin();
+//     setState(() {
+//       _isCreatingPin = _storedPin == null;
+//     });
+//   }
+
+//   // cek apakah pin bener?
+//   void _verifyPin() {
+//     // kalo bener, akan direct ke homepage
+//     if (_pinController.text == _storedPin) {
+//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+//     } else {
+//       // kalo salah, akan muncul text pin salah
+//       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('PIN salah!')));
+//     }
+//   }
+
+//   // menyimpan pin baru lalu direct ke homepage
+//   void _createPin() async {
+//     await PinStorage.setPin(_pinController.text);
+//     Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       // tampilan nya
+//       backgroundColor: Colors.blueAccent,
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               // kl creating maka muncul buat pin, kl ga create maka masukkan pin
+//               Text(
+//                 _isCreatingPin ? 'Buat PIN' : 'Masukkan PIN',
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               TextField(
+//                 controller: _pinController,
+//                 decoration: InputDecoration(
+//                   filled: true,
+//                   fillColor: Colors.white,
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(8),
+//                   ),
+//                   labelText: 'PIN',
+//                 ),
+//                 obscureText: true,
+//               ),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: _isCreatingPin ? _createPin : _verifyPin,
+//                 child: Text(_isCreatingPin ? 'Buat PIN' : 'Masuk'),
+//                 style: ElevatedButton.styleFrom(
+//                   padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+//                   textStyle: TextStyle(fontSize: 18),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'storage.dart';
 import 'homepage.dart';
 
 class PinPage extends StatefulWidget {
+  final bool isChangingPin;
+
+  PinPage({this.isChangingPin = false});
+
   @override
   _PinPageState createState() => _PinPageState();
 }
 
 class _PinPageState extends State<PinPage> {
-  // controller untuk textfield input pin
   final TextEditingController _pinController = TextEditingController();
-  // untuk cek apakah bikin baru? kl ngga brarti
-  bool _isCreatingPin = false;
-
-  // ini, storedpin : masukin pin yang sudah ada
+  final TextEditingController _newPinController = TextEditingController();
   String? _storedPin;
+  bool _isCreatingPin = false;
+  bool _isVerifyingOldPin = false;
 
   @override
   void initState() {
@@ -22,35 +122,40 @@ class _PinPageState extends State<PinPage> {
     _checkStoredPin();
   }
 
-  // pengecekan apakah pin nya null?
   Future<void> _checkStoredPin() async {
     _storedPin = await PinStorage.getPin();
     setState(() {
-      _isCreatingPin = _storedPin == null;
+      if (_storedPin == null) {
+        _isCreatingPin = true; // First run, create a new PIN
+      } else if (widget.isChangingPin) {
+        _isVerifyingOldPin = true; // Changing existing PIN
+      }
     });
   }
 
-  // cek apakah pin bener?
   void _verifyPin() {
-    // kalo bener, akan direct ke homepage
     if (_pinController.text == _storedPin) {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+      if (widget.isChangingPin) {
+        setState(() {
+          _isVerifyingOldPin = false; // Proceed to create new PIN
+          _isCreatingPin = true;
+        });
+      } else {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+      }
     } else {
-      // kalo salah, akan muncul text pin salah
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('PIN salah!')));
     }
   }
 
-  // menyimpan pin baru lalu direct ke homepage
   void _createPin() async {
-    await PinStorage.setPin(_pinController.text);
+    await PinStorage.setPin(_newPinController.text);
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // tampilan nya
       backgroundColor: Colors.blueAccent,
       body: Center(
         child: Padding(
@@ -58,9 +163,12 @@ class _PinPageState extends State<PinPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // kl creating maka muncul buat pin, kl ga create maka masukkan pin
               Text(
-                _isCreatingPin ? 'Buat PIN' : 'Masukkan PIN',
+                _isCreatingPin
+                    ? 'Buat PIN Baru'
+                    : _isVerifyingOldPin
+                        ? 'Masukkan PIN Lama'
+                        : 'Masukkan PIN',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
@@ -69,14 +177,14 @@ class _PinPageState extends State<PinPage> {
               ),
               SizedBox(height: 20),
               TextField(
-                controller: _pinController,
+                controller: _isCreatingPin ? _newPinController : _pinController,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  labelText: 'PIN',
+                  labelText: _isCreatingPin ? 'PIN Baru' : 'PIN',
                 ),
                 obscureText: true,
               ),
@@ -96,4 +204,3 @@ class _PinPageState extends State<PinPage> {
     );
   }
 }
-
